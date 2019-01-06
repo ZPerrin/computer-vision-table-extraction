8c185bfa15d4d4b516e6517fafca87f5bdc33da4
Zebulon <perrinzeb@gmail.com>
2019-01-05 21:44:23 -0700

    -- cleaner implementation

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -4,16 +4,12 @@ import org.apache.pdfbox.rendering.PDFRenderer;
 import org.junit.Test;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 public class test {
 
@@ -27,23 +23,26 @@ public class test {
         File guassianImage = RESOURCES.resolve("guassian.jpg").toFile();
         File sobelImage = RESOURCES.resolve("sobel.jpg").toFile();
 
-        BufferedImage image = convertToImage(testPDF, convertedImage);
+        // pdf to jpg
+        BufferedImage image = convertToImage(testPDF, convertedImage, 1);
 
-
-        BufferedImage guassian = new Kernel(KernelType.GUASSIAN).filter(image);
+        // guassian blur
+        BufferedImage guassian = guassianConvolution(image);
         ImageIO.write(guassian, "jpg", guassianImage);
-        BufferedImage sobel = new Kernel(KernelType.SOBEL_VERTICAL).filter(guassian);
+
+        // sobel edge detection
+        BufferedImage sobel = sobelConvolution(guassian);
         ImageIO.write(sobel, "jpg", sobelImage);
     }
 
-    private BufferedImage convertToImage(File testPDF, File convertedImage
+    private BufferedImage convertToImage(File testPDF, File convertedImage, int page
     ) throws IOException {
         PDDocument document = null;
         try {
 
             document = PDDocument.load(testPDF);
             PDFRenderer renderer = new PDFRenderer(document);
-            BufferedImage image = renderer.renderImage(1, 2, ImageType.GRAY);
+            BufferedImage image = renderer.renderImage(page, 3, ImageType.GRAY);
 
             ImageIO.write(image, "jpg", convertedImage);
 
@@ -59,137 +58,145 @@ public class test {
         return null;
     }
 
+    public BufferedImage guassianConvolution(BufferedImage image) {
 
-    public enum KernelType {
-        SOBEL_HORIZONTAL,
-        SOBEL_VERTICAL,
-        GUASSIAN;
+        int width = image.getWidth();
+        int height = image.getHeight();
+        BufferedImage filteredImage = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);
+
+        for (int x = 0; x < width; x++) {
+
+            for (int y = 0; y < height; y++) {
+
+                int guassValue = convolveGuassianPixel(image, x, y);
+                filteredImage.setRGB(x, y, guassValue);
+            }
+        }
+
+        return filteredImage;
     }
 
-    public class Kernel {
+    public int convolveGuassianPixel(BufferedImage image, int x, int y) {
 
-        private int[][] kernel = new int[3][3];
-        private KernelType type = null;
+        int[][] guassian = Kernel.GUASSIAN_3x3.getKernel();
 
-        public Kernel(KernelType type) {
+        int width = image.getWidth(),
+                height = image.getHeight(),
+                kernelWidth = guassian.length,
+                kernelHeight = guassian[0].length;
 
-            this.type = type;
-            if (KernelType.SOBEL_VERTICAL.equals(type)) {
+        int gValue = 0;
+        for (int i = 0; i < kernelWidth; i++) {
 
-                kernel[0][0] = -1;
-                kernel[0][1] = 0;
-                kernel[0][2] = 1;
-                kernel[1][0] = -2;
-                kernel[1][1] = 0;
-                kernel[1][2] = 2;
-                kernel[2][0] = -1;
-                kernel[2][1] = 0;
-                kernel[2][2] = 1;
-            } else if (KernelType.SOBEL_HORIZONTAL.equals(type)) {
-                kernel[0][0] = -1;
-                kernel[0][1] = -2;
-                kernel[0][2] = 1;
-                kernel[1][0] = 0;
-                kernel[1][1] = 0;
-                kernel[1][2] = 0;
-                kernel[2][0] = -1;
-                kernel[2][1] = 2;
-                kernel[2][2] = 1;
-            } else if (KernelType.GUASSIAN.equals(type)) {
-                kernel[0][0] = 1;
-                kernel[0][1] = 2;
-                kernel[0][2] = 1;
-                kernel[1][0] = 2;
-                kernel[1][1] = 4;
-                kernel[1][2] = 2;
-                kernel[2][0] = 1;
-                kernel[2][1] = 2;
-                kernel[2][2] = 1;
+            int iX = x + 1 - i;
+            for (int j = 0; j < kernelHeight; j++) {
+
+                int iY = y + 1 - j;
+                gValue += (iX >= 0 && iX < width) && (iY >= 0 && iY < height) ? image.getRGB(iX, iY) * guassian[(kernelWidth - 1) - i][(kernelHeight -1) - j] : 0;
             }
         }
 
-        public BufferedImage filter(BufferedImage original) {
+        int d = (kernelWidth * kernelWidth);
+        gValue = gValue / d;
 
-            int width = original.getWidth();
-            int height = original.getHeight();
-            BufferedImage filteredImage = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);
+        return gValue;
+    }
 
-            Set<Integer> values = new HashSet<>();
-            //if (KernelType.SOBEL_VERTICAL.equals(type) || KernelType.SOBEL_HORIZONTAL.equals(type)) {
-            int max = 0;
+    public BufferedImage sobelConvolution(BufferedImage image) {
 
-            for (int x = 0; x < original.getWidth(); x++) {
+        int width = image.getWidth();
+        int height = image.getHeight();
+        BufferedImage filteredImage = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);
 
-                for (int y = 0; y < original.getHeight(); y++) {
+        for (int x = 0; x < width; x++) {
 
-                    int value = 0;
-                    //            System.out.println("(" + x + ", " + y + ")");
+            for (int y = 0; y < height; y++) {
 
-                    // first column
-                    if (x - 1 >= 0) {
-                        value = value + kernel[1][0] * original.getRGB(x - 1, y);
-                    }
-
-                    if (x - 1 >= 0 && y - 1 >= 0) {
-                        value = value + kernel[0][0] * original.getRGB(x - 1, y - 1);
-                    }
-
-                    if (x - 1 >= 0 && y + 1 < original.getHeight()) {
-                        value = value + kernel[2][0] * original.getRGB(x - 1, y + 1);
-                    }
-
-                    // second column
-                    if (y - 1 >= 0) {
-                        value = value + kernel[0][1] * original.getRGB(x, y - 1);
-                    }
-
-                    value = value + kernel[1][1] * original.getRGB(x, y);
-
-                    if (y + 1 < original.getHeight()) {
-                        value = value + kernel[2][1] * original.getRGB(x, y + 1);
-                    }
-
-                    // third column
-                    if (x + 1 < original.getWidth()) {
-                        value = value + kernel[1][2] * original.getRGB(x + 1, y);
-                    }
-
-                    if (x + 1 < original.getWidth() && y - 1 >= 0) {
-                        value = value + kernel[0][2] * original.getRGB(x + 1, y - 1);
-                    }
-
-                    if (x + 1 < original.getWidth() && y + 1 < original.getHeight()) {
-                        value = value + kernel[2][2] * original.getRGB(x + 1, y + 1);
-                    }
-
-                    // average
-                    value = value / 9;
-                    if (value >= max) {
-                        max = value;
-                    }
-
-                    if (KernelType.SOBEL_HORIZONTAL.equals(type) || KernelType.SOBEL_VERTICAL.equals(type)) {
-                        if (value > 0) {
-                            values.add(value);
-                        }
-                       // System.out.println(value & 0xFF);
-
-                        filteredImage.setRGB(x, y, value);
-
-                    } else {
-                        filteredImage.setRGB(x, y, value);
-                    }
+                int sobelValue = convolveSobelPixel(image, x, y);
+                if (getGreyScale(sobelValue) > 0) {
+                    filteredImage.setRGB(x, y, Color.white.getRGB()); // just set every edge to white
                 }
             }
-            //}
-
-            System.out.println(max);
-            List<Integer> test = new ArrayList(values);
-            Collections.sort(test);
-            for (Integer i : test) {
-                System.out.println(i);
-            }
-                return filteredImage;
         }
+
+        return filteredImage;
+    }
+
+    public int convolveSobelPixel(BufferedImage image, int x, int y) {
+
+        int[][] sobelH = Kernel.SOBEL_HORIZONTAL_3x3.getKernel(),
+                sobelV = Kernel.SOBEL_VERTICAL_3x3.getKernel();
+
+        int width = image.getWidth(),
+                height = image.getHeight(),
+                kernelWidth = sobelH.length,
+                kernelHeight = sobelH[0].length;
+
+        int gx = 0;
+        int gy = 0;
+        for (int i = 0; i < kernelWidth; i++) {
+
+            int iX = x + 1 - i;
+            for (int j = 0; j < kernelHeight; j++) {
+
+                int iY = y + 1 - j;
+                gx += (iX >= 0 && iX < width) && (iY >= 0 && iY < height) ? image.getRGB(iX, iY) * sobelH[(kernelWidth - 1) - i][(kernelHeight -1) - j] : 0;
+                gy += (iX >= 0 && iX < width) && (iY >= 0 && iY < height) ? image.getRGB(iX, iY) * sobelV[(kernelWidth - 1) - i][(kernelHeight -1) - j] : 0;
+            }
+        }
+
+        int d = (kernelWidth * kernelWidth);
+        gx = gx / d;
+        gy = gy / d;
+
+        return (int) Math.sqrt((gx * gx) + (gy * gy));
+    }
+
+    public enum Kernel {
+
+        SOBEL_HORIZONTAL_3x3,
+        SOBEL_VERTICAL_3x3,
+        GUASSIAN_3x3;
+
+        // note, rows here are columns in the matrix
+        private int[][] SOBELV_3X3 = {
+                {-1, 0, 1},
+                {-2, 0, 2},
+                {-1, 0, 1}
+        };
+        private int[][] SOBELH_3X3 = {
+                {-1, -2, -1},
+                {0, 0, 0},
+                {1, 2, 1}
+        };
+        private int[][] GUASS_3X3 = {
+                {1, 2, 1},
+                {2, 4, 2},
+                {1, 2, 1}
+        };
+
+        public int[][] getKernel() {
+
+            if (SOBEL_VERTICAL_3x3.equals(this)){
+                return SOBELV_3X3;
+            } else if (SOBEL_HORIZONTAL_3x3.equals(this)) {
+                return SOBELH_3X3;
+            } else if (GUASSIAN_3x3.equals(this)) {
+                return GUASS_3X3;
+            }
+            return null;
+        }
+    }
+
+    private int getGreyScale(int rgb) {
+
+        int r = (rgb >> 16) & 0xff;
+        int g = (rgb >> 8) & 0xff;
+        int b = (rgb) & 0xff;
+
+        // from https://en.wikipedia.org/wiki/Grayscale - calculating luminance
+        int gray = (int)(0.2126 * r + 0.7152 * g + 0.0722 * b);
+
+        return gray;
     }
 }
